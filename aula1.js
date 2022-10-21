f008aefeb95b79f34238a61f6eb6148c9e19519d
Ben-Hur <benhurcarvalho2@gmail.com>
2022-10-21 10:27:25 -0300

    aula 1

---
+++ aula1.js
@@ -0,0 +1,23 @@
+// Faça um programa para calcular o valor de uma viagem.
+
+//     Você terá três variáveis, sendo elas:
+    
+//         1 - preço do combustível;
+//         2 - gasto médio de combustível por Km;
+//         3 - distância em KM da viagem;
+
+// Imprima no console o gasto total com a viagem:
+
+const fuelPrice = 5.79;
+const priceKm = 10;
+const distanceKm = 193;
+function valorGasto () {
+
+const listrosGasto = distanceKm / priceKm;
+const valorGasto = listrosGasto * fuelPrice;
+return valorGasto.toFixed(2)
+}
+
+
+console.log(valorGasto());
+
